409b0853a52aeb6360b165f63099c22c12cba343
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2024-09-04 15:43:33 +0700

    Improve

---
+++ ScriptableObject/ADataItem.2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SisyphusLab.Utils
+{
+    public abstract class ADataItem<T, TEnum>
+    where TEnum : System.Enum
+    {
+        public TEnum Type;
+        public T Data;
+    }
+
+}
---
+++ ScriptableObject/AScriptableDatabase.2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SisyphusLab.Utils
+{
+    public abstract class AScriptableDatabase<T, TEnum, T1> : ScriptableObject
+        where TEnum : System.Enum 
+        where T : ADataItem<T1, TEnum>
+
+    {
+        [SerializeField] protected List<T> _data;
+
+        Dictionary<TEnum, T1> _dataDict = new();
+
+        private void OnEnable()
+        {
+            _dataDict.Clear();
+            foreach (var item in _data)
+            {
+                if (_dataDict.ContainsKey(item.Type))
+                    continue;
+                _dataDict.Add(item.Type, item.Data);
+            }
+        }
+
+
+
+        public T1 GetByType(TEnum type)
+        {
+            return _dataDict[type];
+        }
+    }
+
+}
