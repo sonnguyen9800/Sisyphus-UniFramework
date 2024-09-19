7caa98582ab2f2c6d45458dc0263bbe2578242a5
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2024-09-20 00:38:45 +0700

    add comment

--- Notifier/IObserver.cs
+++ Notifier/IObserver.cs
@@ -2,6 +2,12 @@
 
 namespace SisyphusLab.Notifier
 {
+    /// <summary>
+    /// IObserver with Param support. Command is supplied
+    /// as an enum
+    /// </summary>
+    /// <typeparam name="T">Command sent</typeparam>
+    /// <typeparam name="TParam">Param</typeparam>
     public interface IObserver<in T, TParam>
     {
         void OnCompleted();
